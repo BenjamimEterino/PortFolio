55be6251167cd96ab33525d4ea3fb2b04a6f31cb
BenjamimEterino <benjamimtoretto901@gmail.com>
2022-01-29 19:25:52 +0200

    Menu a funcionar

--- js/script.js
+++ js/script.js
@@ -3,10 +3,5 @@ function myFunction(x) {
   }
 
   function changeMenu() {
-    var x = document.getElementById("navi")
-    if (x.style.display === "none") {
-      x.style.display = "block"
-    } else {
-      x.style.display = "none"
-    }
+    document.getElementById("navi").classList.toggle("show")
   }
