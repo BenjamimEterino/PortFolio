c2b1a39ca167e0a94d2e17070aafa08db17afb0f
BenjamimEterino <benjamimtoretto901@gmail.com>
2022-01-29 16:12:49 +0200

    Menu commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function myFunction(x) {
-    x.classList.toggle("change");
+    x.classList.toggle("change")   
+  }
+
+  function changeMenu() {
+    var x = document.getElementById("navi")
+    if (x.style.display === "none") {
+      x.style.display = "block"
+    } else {
+      x.style.display = "none"
+    }
   }
